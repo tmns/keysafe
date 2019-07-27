472c12d0505b3956dc6c2f02dcbed01abbb22ee9
sairim <spiri@protonmail.com>
2019-07-27 15:00:34 +0300

    implement register user route

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -12,4 +12,39 @@ const router = express.Router();
 // @access Public
 router.get('/test', (req, res) => res.json({ msg: 'users works' }));
 
+// @route POST api/users/register
+// @desc Register user
+// @access Public
+router.post('/register', async (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  const user = await User.findOne({ username: req.body.username });
+  
+  if (user) {
+    return res.status(400).json({ username: 'Username already exists' });
+  } else {
+    const newUser = new User({
+      username: req.body.username,
+      password: req.body.password
+    });
+
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newUser.password, salt, async (err, hash) => {
+        if (err) throw err;
+        newUser.password = hash;
+        try {
+          const user = await newUser.save();
+          res.json(user);
+        } catch(err) {
+          console.log(err);
+        }
+      })
+    })
+  }
+})
+
 export default router;
