f0964213be10213c6851f445fbba14db260b0547
sairim <spiri@protonmail.com>
2019-08-03 14:22:23 +0300

    add proxy settings for prod

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
-import express from 'express';
-import session from 'express-session';
-import bodyParser from 'body-parser';
-import path from 'path';
-import cors from 'cors';
+import express from "express";
+import session from "express-session";
+import bodyParser from "body-parser";
+import path from "path";
+import cors from "cors";
 
-import * as config from './config/config';
-import { connect } from './db';
-import users from './routes/api/users';
-import groups from './routes/api/groups';
+import * as config from "./config/config";
+import { connect } from "./db";
+import users from "./routes/api/users";
+import groups from "./routes/api/groups";
 
-let MongoDBStore = require('connect-mongodb-session')(session);
+let MongoDBStore = require("connect-mongodb-session")(session);
 
 async function start() {
   try {
@@ -20,37 +20,53 @@ async function start() {
 
   const app = express();
 
+  // disable leaking info of what server we're using
+  app.disable("x-powered-by");
+
+  // to force express to recognize connection as HTTPS and receive cookie with 'secure' set
+  app.set("trust proxy", 1);
+
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
-  
+
   let store = new MongoDBStore({
     uri: config.DB_URI,
-    collection: 'sessions'
+    collection: "sessions"
   });
 
-  store.on('error', function(error) {
+  store.on("error", function(error) {
     console.log(error);
-  })
+  });
 
-  app.use(session({
-    name: config.SESS_NAME,
-    secret: config.SESS_SECRET,
-    resave: true,
-    saveUninitialized: false,
-    cookie: {
-      secure: process.env.NODE_ENV == 'production',
-      maxAge: config.SESS_LIFETIME
-    },
-    store
-  }));
+  app.use(
+    session({
+      name: config.SESS_NAME,
+      secret: config.SESS_SECRET,
+      resave: true,
+      saveUninitialized: false,
+      cookie: {
+        secure: process.env.NODE_ENV == "production",
+        maxAge: config.SESS_LIFETIME
+      },
+      store
+    })
+  );
 
-  app.use('/api/users', users);
-  app.use('/api/groups', groups);
-    
+  app.use("/api/users", users);
+  app.use("/api/groups", groups);
+
+  // serve static assets if in prod
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "public")));
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "public", "index.html"));
+    });
+  }
+
   app.listen(config.SERVER_PORT, err => {
     if (err) throw err;
     console.log(`> Running on localhost:${config.SERVER_PORT}`);
-  });  
+  });
 }
 
 export default start;
